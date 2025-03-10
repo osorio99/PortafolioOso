46d49a8d334647e1b5ce588b4db757368c4be6e2
esteban osorio <osorioosorioesteban@gmail.com>
2025-03-09 22:20:55 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from './assets/layauts/Header/Header'
+
+const App = () => {
+  return (
+    <>
+    <Header/>
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/layauts/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import "./Navbar.css"
+
+const Navbar = () => {
+  return (
+    <nav className="Navbar">
+        <ul>
+            <li><a href="">HOME </a></li>
+            <li><a href="">ABOUT </a></li>  
+            <li><a href="">SKILLS</a></li>
+            <li><a href="">CONTACT</a></li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Navbar
