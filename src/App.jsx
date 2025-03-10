4c26c43e9b642fd46619a4fd7274730f27a29a53
esteban osorio <osorioosorioesteban@gmail.com>
2025-03-10 01:15:43 -0500

    create main,section,footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
-import Header from './assets/layauts/Header/Header'
-
+import Header from './layauts/Header/Header'
+import img from "/src/assets/images/photo.jpg"
+import Footer from './layauts/Footer/Footer'
+import "./App.css"
 const App = () => {
   return (
     <>
     <Header/>
+
+      <main>
+          <section className='contain'>
+            <div className='box1'>
+              <h2>info</h2>
+            <p>vendo mariguana a buen precio 
+              por que lo intento si se q no puedo hacer nada bien en la vida siempre me equivoco una y otra y otra vez y sigo como si no hubiera pasado nada por eso me dejan por inservible por conformarme por pensar que todo esta bien por creer que puedo hacer algo y nunca hago nada la forma en creo que me ven es como hablador perezoso alguien que no tiene propocitos conformista mediocre 
+            </p>
+            </div>
+            <div className='box2'>
+            <img src={img} alt="photo" className='img'/>
+            </div>
+          </section>
+          </main> 
+
+          <Footer/>
     </>
   )
 }
--- src/layauts/Header/Header.jsx
+++ src/layauts/Header/Header.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import "./Header.css"
+//import img from "../../images/photo.jpg"
 import Navbar from '../Navbar/Navbar'
 const Header = () => {
   return (
     
-
+      <>
         <header className='header'>
             <h1>Esteban osorio</h1>
            <Navbar/>
         </header>
 
-
+              
+      </>
   
   )
 }
