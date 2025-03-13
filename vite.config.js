84177b4635330ec2505149b886af35a72578a835
esteban osorio <osorioosorioesteban@gmail.com>
2025-03-12 22:07:04 -0500

    despliege

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/PortafolioOso.git/', // Reemplaza con el nombre exacto de tu repositorio
 })
