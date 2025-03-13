612ec61c176f04e4a144b5c3c0d2df83f4a4ce48
esteban osorio <osorioosorioesteban@gmail.com>
2025-03-13 00:44:57 -0500

    falta organizar el about

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,9 @@ const App = () => {
     <Header/>
     
         <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/Home" element={<Home/>} /> 
-        <Route path="/About" element={<About/>} />
+        
+        <Route path="/Home" element={ <Home/> } /> 
+        <Route path="/About" element={ <About/> } />
        
       </Routes>
      
--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import { ItemNavbar } from '../../componets/ItemNavbar'
+import { FaHtml5 } from "react-icons/fa6";
+import { FaCss3Alt } from "react-icons/fa6";
+import { IoLogoJavascript } from "react-icons/io5";
+import { FaReact } from "react-icons/fa6";
+import "./About.css"
 const About = () => {
   return (
     <div>
         <div>
           <h2>Habilidades Técnicas</h2>
             <ul>
-            <ItemNavbar contentItem={" HTML"}/>
-            <ItemNavbar contentItem={"CSS"}/>
-            <ItemNavbar contentItem={"JavaScript"}/>
-            <ItemNavbar contentItem={" React "}/>
+            <ItemNavbar contentItem={<FaHtml5 style={{color:"#E34F26", height:"40px"}} />}/>
+            <ItemNavbar contentItem={<FaCss3Alt style={{color:"#1572B6", height:"40px"}}/>}/>
+            <ItemNavbar contentItem={<IoLogoJavascript style={{color:" #F7DF1E", height:"40px"}} />}/>
+            <ItemNavbar contentItem={<FaReact style={{color:" #61DAFB", height:"40px"}} /> }/>
 
             </ul>
 
@@ -17,9 +22,12 @@ const About = () => {
 
         <h2>Habilidades Sociales</h2>
 
-
+      <ul>
+      <p> Empatía, Trabajo en equipo, Asertividad,Resolución de conflictos</p>
+      </ul>
 
         <h2>Nivel de Inglés</h2>
+        <ul><h3>A2</h3></ul>
     </div>
   )
 }
